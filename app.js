e32037e36b0fe468079fe67c994e5bb512a90782
panghwi <kanean99@naver.com>
2022-07-21 17:08:10 +0900

    add new api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello, world!'));
+app.get('/delivery', (req, res) => res.send('OK'));
 app.get('/error', (req, res) => res.status(500).send('Internal Server Error'));
 
 app.listen(port, () =>
