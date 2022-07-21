f883d7daaf28798d234d9b834fefda2a4e38be46
panghwi <kanean99@naver.com>
2022-07-21 17:06:33 +0900

    add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import { log } from 'console';
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello, world!'));
+app.get('/error', (req, res) => res.status(500).send('Internal Server Error'));
+
+app.listen(port, () =>
+  console.log(`Test app listening at http://localhost:${port}`)
+);
